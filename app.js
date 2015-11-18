46dcc01bf7d82f26ba653cf16d904b600a2c7ffd
JamesMarcogliese <james.marcogliese@gmail.com>
2015-11-18 11:07:34 -0500

    Added sounds and room functionality

--- app.js
+++ app.js
@@ -5,21 +5,31 @@ var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 app.use(express.static(__dirname + '/public'));
 
+//Sets up dir for static file retrieval.
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
-
+//On connection to socket, join default room.
 io.sockets.on('connection', function(socket){
+  socket.room = 'Alfa';
+  socket.join('Alfa');
   console.log('user connected.');
+  console.log('Joined room: ' + socket.room);
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  socket.on('morse signal', function(msg){
-	socket.broadcast.emit('morse signal', msg);
-    //io.emit('morse signal', msg);
+  //Changes wire, triggered by client call.
+  socket.on('change wire', function(newroom) {
+	socket.leave(socket.room);
+	socket.room = newroom;
+	socket.join(newroom);
+	console.log('Changed room to: ' + newroom);
+  });
+  //Sends signal to other users in room/wire.
+  socket.on('send morse signal', function(msg){
+	socket.broadcast.to(socket.room).emit('receive morse signal', msg);
+	console.log(msg.room);
 	console.log('message: ' + msg);
   });
 });
-
-var rooms = ['room1','room2','room3','room4','room5','room6'];
 
